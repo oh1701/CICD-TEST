0a2a98d5927659afd44ac8c4f862b91d2635af2e
오규성 <oh1701>
2023-09-18 10:22:06 +0900

    .

--- app/src/main/java/gyu/practice/cicdtest/MainActivity.kt
+++ app/src/main/java/gyu/practice/cicdtest/MainActivity.kt
@@ -2,10 +2,15 @@ package gyu.practice.cicdtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import gyu.practice.cicdtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
